74e6e8d7475a06c4a4566cf566c5a373d8b4c1b1
Matthew Hoffman <mhoffman@udel.edu>
2015-03-15 18:49:11 -0400

    added xlrd to requirements for pip, formatted FileUtility.py

--- Utility/FileUtility.py
+++ Utility/FileUtility.py
@@ -9,28 +9,29 @@ import xlrd
 
 from re import sub
 
+
 class Instance(object):
- 
+
     """
-    A data instances given data type headers and the corresponding values. 
+    A data instances given data type headers and the corresponding values.
     Instances are stored in a dictionary (Instance.Data) where the headers are keys
     """
 
     def __init__(self, header=[], values=[]):
         self.Data = {}
-        for i in range(0,len(header)): 
+
+        for i in range(0, len(header)):
             self.Data[header[i]] = values[i]
 
     def __repr__(self):
         return str(self.Data)
 
 
-
 def getDataFrom(path):
 
     """
-    performs the simple tasks of reading in data instances. 
-    Returns a tuple of data instances and the corresponding column headers 
+    performs the simple tasks of reading in data instances.
+    Returns a tuple of data instances and the corresponding column headers
     """
 
     columns = []     # the headers
@@ -38,7 +39,7 @@ def getDataFrom(path):
 
     with open(str(path), 'r') as dataFile:
         columns = dataFile.readline().strip().split(',')
-        for nextLine in dataFile: 
+        for nextLine in dataFile:
             d = nextLine.strip().split(',')
             n = Instance(columns, d)
             instances.append(n)
@@ -53,25 +54,25 @@ def XLStoCSV(path):
     """
 
     with xlrd.open_workbook(path) as wb:
-        n = wb.nsheets #worksheets
+        n = wb.nsheets  # worksheets
         # convert each sheet in wb to its own csv
         for i in range(0, n):
-            doc = wb.sheet_by_index(i) #current worksheet
-            r = doc.nrows #rows
- 
-            csv = open(sub('\.xls[x]$','',path) + doc.name + '.csv', 'w')
-            
+            doc = wb.sheet_by_index(i)  # current worksheet
+            r = doc.nrows  # rows
+
+            csv = open(sub('\.xls[x]$', '', path) + doc.name + '.csv', 'w')
+
             for j in range(0, r):
-               row_str = ""
+                rowString = ""
 
-               for cell in doc.row_values(j):
-                  #need to change the values from floats to match the original data
-                  if type(cell) == float and cell % 1 == 0:
-                      cell = int(cell)
-                  row_str += str(cell) + ','
+                for cell in doc.row_values(j):
+                    # need to change the values from floats to match the original data
+                    if type(cell) == float and cell % 1 == 0:
+                        cell = int(cell)
+                    rowString += str(cell) + ','
 
-               #remove trailing ',' and write
-               csv.write(row_str[0:len(row_str)-2] + '\n')
-            
+                # remove trailing ',' and write
+                csv.write(rowString[0:len(rowString)-2] + '\n')
+
             wb.unload_sheet(i)
             csv.close()
