23cd08b1ec5410538ccdcc0124fb8b4843e3442b
Brandon Trautmann <btraut@udel.edu>
2015-03-15 00:17:29 -0400

    comments for easier analysis

--- UI/mainui.py
+++ UI/mainui.py
@@ -13,21 +13,25 @@ class Example(QtGui.QMainWindow):
         
     def initUI(self):               
 
+        # set up exit action and its props
         exitAction = QtGui.QAction(QtGui.QIcon('run.png'), '&XLStoCSV', self)
         exitAction.setShortcut('Ctrl+Q')
-        exitAction.setStatusTip('Run Kelly\'s Script')
         exitAction.triggered.connect(self.close)
-
+        # status for status bar
+        exitAction.setStatusTip('Run Kelly\'s Script')
         self.statusBar()
 
+        # create the menu bar, by default on Mac OS it is in top menu bar
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAction)
 
+        # add the toolbar and stick action on it
         toolbar = self.addToolBar('XLStoCSV')
         toolbar.addAction(exitAction)
         
-        self.setGeometry(300, 300, 700, 500)
+        # window props
+        self.setGeometry(300, 300, 700, 500) # needs to not be hard-coded
         self.setWindowTitle('Data Visualization')    
         self.show()
         
