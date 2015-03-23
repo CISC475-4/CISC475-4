8c1fa319c8759a30a9b304adc1ba1cdc203d7a12
Andrew Shearer <aishear@udel.edu>
2015-03-22 23:41:51 -0400

    Added example graphs to the main gui

--- ui/mainui.py
+++ ui/mainui.py
@@ -2,9 +2,68 @@
 # -*- coding: utf-8 -*-
 
 import sys
-from PySide import QtGui
+import random
+from PySide import QtGui, QtCore
 import viztoolbar
 import vizmenubar
+from numpy import arange, sin, pi
+import matplotlib
+matplotlib.use('Qt4Agg')
+matplotlib.rcParams['backend.qt4']='PySide'
+from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+# copied from http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
+class MyMplCanvas(FigureCanvas):
+    """Ultimately, this is a QWidget (as well as a FigureCanvasAgg, etc.)."""
+    def __init__(self, parent=None, width=5, height=4, dpi=100):
+        fig = Figure(figsize=(width, height), dpi=dpi)
+        self.axes = fig.add_subplot(111)
+        # We want the axes cleared every time plot() is called
+        self.axes.hold(False)
+
+        self.compute_initial_figure()
+
+        #
+        FigureCanvas.__init__(self, fig)
+        self.setParent(parent)
+
+        FigureCanvas.setSizePolicy(self,
+                                   QtGui.QSizePolicy.Expanding,
+                                   QtGui.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def compute_initial_figure(self):
+        pass
+
+
+# copied from http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
+class MyStaticMplCanvas(MyMplCanvas):
+    """Simple canvas with a sine plot."""
+    def compute_initial_figure(self):
+        t = arange(0.0, 3.0, 0.01)
+        s = sin(2*pi*t)
+        self.axes.plot(t, s)
+
+
+# copied from http://matplotlib.org/examples/user_interfaces/embedding_in_qt4.html
+class MyDynamicMplCanvas(MyMplCanvas):
+    """A canvas that updates itself every second with a new plot."""
+    def __init__(self, *args, **kwargs):
+        MyMplCanvas.__init__(self, *args, **kwargs)
+        timer = QtCore.QTimer(self)
+        timer.timeout.connect(self.update_figure)
+        timer.start(1000)
+
+    def compute_initial_figure(self):
+        self.axes.plot([0, 1, 2, 3], [1, 2, 0, 4], 'r')
+
+    def update_figure(self):
+        # Build a list of 4 random integers between 0 and 10 (both inclusive)
+        l = [random.randint(0, 10) for i in range(4)]
+
+        self.axes.plot([0, 1, 2, 3], l, 'r')
+        self.draw()
 
 
 class MainScreen(QtGui.QMainWindow):
@@ -19,9 +78,10 @@ class MainScreen(QtGui.QMainWindow):
         Input:
         controller (Controller instance) initialized instance of the controller
         """
-        super(MainScreen, self).__init__()
 
+        super(MainScreen, self).__init__()
         self.controller = controller
+        self.main_widget = QtGui.QWidget(self)
 
         # Set up the initial UI
         self.init_ui()
@@ -42,6 +102,17 @@ class MainScreen(QtGui.QMainWindow):
         # window props
         self.setWindowTitle('Data Visualization')
         self.showMaximized()   
+
+
+        # set-up matplotlib canvas
+        l = QtGui.QVBoxLayout(self.main_widget)
+        sc = MyStaticMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+        dc = MyDynamicMplCanvas(self.main_widget, width=5, height=4, dpi=100)
+        l.addWidget(sc)
+        l.addWidget(dc)
+
+        self.main_widget.setFocus()
+        self.setCentralWidget(self.main_widget)
 
     def setup_actions(self):
 
