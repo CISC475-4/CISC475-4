eab971f96ec67af76a637cfa3fdc546773b0d485
delucks <jamie.delucks@gmail.com>
2015-04-12 17:48:45 -0400

    Readying the code to be pulled into master

--- main.py
+++ main.py
@@ -14,14 +14,17 @@ from controller.controller import Controller
 def main():
     # Initialize the controller
     main_controller = Controller()
-    main_controller.setup_db()
-    main_controller.import_file_to_database('Output_20274_Training_D1_.xlsx')
-    main_controller.import_file_to_database('Output_40323_Training_D1_.xlsx')
+    ### This block of commented-out code shows you how to use our DB!
+    ### This method should be called before any DB interactions
+    #main_controller.setup_db()
+    ### This is how you load a specific file into the DB 
+    #main_controller.import_file_to_database('Output_20274_Training_D1_.xlsx')
+    #main_controller.import_file_to_database('Output_40323_Training_D1_.xlsx')
 
-    #application = QtGui.QApplication(sys.argv)
-    ## Initialize the MainScreen (main window of the UI) and pass it our controller
-    #this_screen = MainScreen(main_controller)
-    #sys.exit(application.exec_())
+    application = QtGui.QApplication(sys.argv)
+    # Initialize the MainScreen (main window of the UI) and pass it our controller
+    this_screen = MainScreen(main_controller)
+    sys.exit(application.exec_())
 
 
 if __name__ == '__main__':
