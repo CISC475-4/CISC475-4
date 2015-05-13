8234e3adb22588d86ec33263967021a60d787a18
Ryan Keeley <ryan7keeley@gmail.com>
2015-05-13 19:57:07 -0400

    Completed some tests and fixed others. Some still failing.

--- tests/test_database.py
+++ tests/test_database.py
@@ -14,12 +14,16 @@ import logging
 
 class TestDatabase(unittest.TestCase):
     """Test the functions in the DatabaseManager class."""
+
+    # This is here so these dbs don't connect to the same or default db
+    test_address = ''
+
 #    def setUp(self):
 #        """initializes data necessary to test database functionality"""
 #	pass
 
     def test_connect(self):
-	db = database.DatabaseManager()
+	db = database.DatabaseManager(self.test_address)
 	sql_conn_orig = db.sql_conn
 	cursor_orig = db.cursor
 	db.connect()
@@ -30,37 +34,44 @@ class TestDatabase(unittest.TestCase):
 	self.assertEqual(db.sql_conn.row_factory, sqlite3.Row)
 
     def test_disconnect(self):
-        db = database.DatabaseManager()
+        db = database.DatabaseManager(self.test_address)
 	db.connect()
         db.disconnect()
         self.assertIsNone(db.cursor)
     
     def test_setup(self):
-	"""I'm not sure if I understand what setup() is supposed to do"""
-        db = database.DatabaseManager()
-	db.connect()
-	db.setup() # No input sql_filename
+        db_setup = database.DatabaseManager(self.test_address)
+	db_setup.connect()
+	changes = db_setup.setup()
 	
-	# How to test logging messages?
-	# There should be new data in the database if the setup was successful
-	# Maybe table creations don't count as db changes?
-	self.assertGreater(db.sql_conn.total_changes, 0)
+        # Table creations don't count as changes, so that can't be checked.
+        # Instead, just make sure check_db_setup works as planned.
+        self.assertTrue(db_setup.check_db_setup())
 
-	# No error messages for files that don't exist?
-	db2 = database.DatabaseManager()
-	db2.connect()
-	self.assertRaises(IOError, db2.setup, 'fileThatDoesntExist')
+        # Database that tries to import a file that doesn't exist
+	db_bad_fname = database.DatabaseManager(self.test_address)
+	db_bad_fname.connect()
+	self.assertRaises(IOError, db_bad_fname.setup, 'fileThatDoesntExist')
+        self.assertFalse(db_bad_fname.check_db_setup())
 
     def test_check_db_setup(self):
-        db_setup = database.DatabaseManager()
+        """ checks to see that (un)initialized dbs are recognized as such """
+        # db_setup will check if a set-up db is accepted
+        db_setup = database.DatabaseManager('test')
 	db_setup.connect()
 	db_setup.setup()
-	db_not_setup = database.DatabaseManager()
-
 	self.assertTrue(db_setup.check_db_setup())
-	self.assertFalse(db_not_setup.check_db_setup())
+        db_setup.disconnect()
+
+        # db_not_setup will test to see that uninitialized dbs aren't accepted
+        # Different name so the dbs in this function are definitely different
+	db_not_setup = database.DatabaseManager('')
+        db_not_setup.connect()
+        # don't call db_not_setup.setup()
+	self.assertFalse(db_not_setup.check_db_setup()) 
 
     def test_import_file_to_database(self):
-        db = database.DatabaseManager()
+        db = database.DatabaseManager(self.test_address)
 	self.assertRaises(IOError, db.import_file_to_database, 'fakeFileName')
-        # @TODO: Finish this function
+
+        # Jamie's fixing this right now, so I'll do it later
